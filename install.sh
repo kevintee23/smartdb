b5356a6757ade83e667f4a48e7b73815ecf93fcf
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-04-29 12:16:15 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install script for cameraTest
+# by de_man
+# 23/4/2019
+
+cd
+
+echo '[+] Installing and updating core dependencies...'
+sudo apt-get update && sudo apt-get -y upgrade
+
+echo '[+] Installing pip, gunicorn and AWS CLI...'
+sudo apt-get install python-pip gunicorn awscli
+
+echo '[+] Installing flask...'
+sudo pip install flask
+
+echo '[+] Installing required packages...'
+sudo pip install boto3 watchdog simplejson PiCamera
+rm -rf ~/.cache/pip
+
+echo '[+] Cloning gunicorn service file to the appropriate folder'
+sudo cp /home/pi/cameraTest/cameraTest.service /etc/systemd/system/
+
+echo '[+] Setting up permissions...'
+cd cameratest
+chmod +x takepicture.sh
+
+echo '[+] Configure AWS...'
+aws configure
+
+echo 'May the force be with you...'
