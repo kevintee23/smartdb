c36bc119a7efce2a6d72e845f782cbce21f165d3
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-04 10:40:21 +1000

    Update hook.py

--- hook.py
+++ hook.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def speedTest():
    return subprocess.Popen('./takepicture.py', shell=True, stdout=subprocess.PIPE).stdout.read()
+
+@app.route('/smartdb/static/')
+@app.route('/smartdb/static/<name>')
+def hello(name=None):
+    return render_template('picture.html', name=name)
