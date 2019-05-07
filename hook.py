80fcd485409e88aba3bdc8be4f3574e5416148d5
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-07 10:49:09 +1000

    Update hook.py
    Added route to media to display picture gallery

--- hook.py
+++ hook.py
@@ -5,11 +5,20 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
+#Will execute the takepicture.py script when x.x.x.x:5000 is accessed
 @app.route('/')
 def speedTest():
    return subprocess.Popen('./takepicture.py', shell=True, stdout=subprocess.PIPE).stdout.read()
 
+#Will display the captured pic - this is to work with Pushover notification where the pic will be displayed in the notification.
 @app.route('/smartdb/static/')
 @app.route('/smartdb/static/<name>')
 def hello(name=None):
     return render_template('picture.html', name=name)
+
+#Will display all captured images by accessing x.x.x.x:5000/media
+@app.route('/media')
+def media():
+	hists = sorted(os.listdir('/home/pi/smartdb/static'), reverse=True)
+	hists = [file for file in hists]
+	return render_template('gallery.html', hists=hists)
