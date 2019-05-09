51d3d62417b0c4575a5ecdec7d6ca18079ac8ea3
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-09 10:57:18 +1000

    Update install.sh
    Added colors to info

--- install.sh
+++ install.sh
@@ -5,7 +5,18 @@
 # Run this script on /home/pi folder only
 # 4/5/2019
 
-#wgethttps://raw.githubusercontent.com/kevintee23/smartdb/master/install.sh
+#wget https://raw.githubusercontent.com/kevintee23/smartdb/master/install.sh
+
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
 
 #Informational only, getting your IP Address
 ip=$(hostname -I | cut -f1 --delimiter=' ')
@@ -13,116 +24,116 @@ echo "Your Raspberry Pi IP Address is $ip"
 
 cd
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : Installing and updating core dependencies...
 -------------------------------------------------------------
-"
+$Color_Off"
 sudo apt-get update && sudo apt-get -y upgrade
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Freeing up space. Removing Wolfram-Engine...
 -------------------------------------------------------------
-"
+$Color_Off"
 wolfram="y"
 nowolfram="y"
-echo "Would you like to free up some space by removing the Wolfram-Engine?"
+echo "$Green Would you like to free up some space by removing the Wolfram-Engine? $Color_Off"
 read "Perform space saver routine? Enter y for yes or n for no (default= $wolfram): " nowolfram
 [ -n "$nowolfram" ] && wolfram=$nowolfram
 
 if [ "$wolfram" = "y" ];
 then
-    echo "Removing Wolfram-Engine..."
+    echo "$Cyan Removing Wolfram-Engine... $Color_Off"
     sudo apt-get purge wolfram-engine
 else
-    echo "Skipping this step and moving on..."
+    echo "$Cyan Skipping this step and moving on... $Color_Off"
     
 fi
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Installing pip from pypa.io...
 -------------------------------------------------------------
-"
+$Color_Off"
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo python3 get-pip.py
 
-echo "
+echo "$Cyan
 ---------------------------------------------------------------------
 INFO  : $STATUS Installing gunicorn, cause who doesn't love unicorns!
 ---------------------------------------------------------------------
-"
+$Color_Off"
 sudo apt-get install -y gunicorn
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Installing AWS CLI...
 -------------------------------------------------------------
-"
+$Color_Off"
 sudo apt-get install -y awscli
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Installing Flask webserver...
 -------------------------------------------------------------
-"
+$Color_Off"
 sudo python -m pip install flask
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Installing boto3, the AWS rekognition client...
 -------------------------------------------------------------
-"
+$Color_Off"
 sudo python -m pip install boto3
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Installing requests...
 -------------------------------------------------------------
-"
+$Color_Off"
 sudo python -m pip install requests
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Installing simplejson...
 -------------------------------------------------------------
-"
+$Color_Off"
 sudo python -m pip install simplejson
 rm -rf ~/.cache/pip
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : Cloning gunicorn service file to the appropriate folder
 -------------------------------------------------------------
-"
+$Color_Off"
 cd home/pi/smartdb
 sudo chmod 755 gunicorn.service
 cd
 sudo cp /home/pi/smartdb/gunicorn.service /etc/systemd/system/
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : Creating a folder for all your pictures...
 -------------------------------------------------------------
-"
+$Color_Off"
 cd
 mkdir /home/pi/smartdb/static
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : Setting up permissions...
 -------------------------------------------------------------
-"
+$Color_Off"
 chmod +x /home/pi/smartdb/takepicture.py
 sudo chown -R pi:pi /home/pi/smartdb/*
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : Setting up daemon to run in the background...
 -------------------------------------------------------------
-"
+$Color_Off"
 cd /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
@@ -130,7 +141,7 @@ sudo systemctl enable gunicorn
 
 fi
 
-echo "
+echo "$Cyan
 -------------------------------------------------------------
 INFO  : $STATUS Completed!! Just a few more things...
 -------------------------------------------------------------
@@ -151,6 +162,6 @@ A few things before you start.
     - sudo systemctl daemon-reload
     - sudo systemctl start gunicorn
     - sudo systemctl enable gunicorn
-"
+$Color_Off"
 
 exit 0
