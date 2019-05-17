2597181c8256e94a7c2e2bfd793bd39750f9b312
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-17 11:02:20 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# User Configuration variable settings for smartdb
+# Purpose - Global setting for certain specific parameters
+# Updated - 17-May-2019
+# Done by - Kevin Tee
+
+#======================================
+#   takepicture.py Settings
+#======================================
+
+
+fport = 5000                  # default= 5000 The webserver port that the script will run on
+wcurl = "https://graph-"      # The webCoRE piston URL that the script will send the results of a face match to
+POtoken = "yourTokenHere"     # Your Pushover account Token ID
+POuser = "yourUserKey"        # Your Pushover User Key
