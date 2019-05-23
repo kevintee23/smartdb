7da7ddf57d0e0a587ae87fc8e2b424e3cd5778ad
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-23 17:13:35 +1000

    Update config.py
    converted integer to string for picQuality and fport

--- config.py
+++ config.py
@@ -8,13 +8,13 @@
 #======================================
 
 
-fport = 5000                  # default= 5000 The webserver port that the script will run on
+fport = '5000'                # default= 5000 The webserver port that the script will run on
 wcurl = 'https://graph-'      # The URL of your smart home piston/rule that the script will send the results of a face match to
 POtoken = 'yourTokenHere'     # Your Pushover account Token ID
 POuser = 'yourUserKey'        # Your Pushover User Key
 ifturl = 'iftttUrlHere'       # Your IFTTT URL
 iftEvent = 'smartdb'          # The event name to trigger your IFTTT Maker account
-picQuality = 15               # Value range is between 10-100 (default:20)
+picQuality = '15'             # Value range is between 10-100 (default:20)
 vflip = '-vf'                 # To vertical flip your picture enter "-vf", otherwise leave blank ("")
 hflip = '-hf'                 # To horizontally flip your picture enter "-hf", otherwise leave blank ("")
 
