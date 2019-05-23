340bfb23f2e8892851fbff42deb0e133a50670d1
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-23 11:48:15 +1000

    Update config.py
    changed default picQuality from 20 to 15

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ wcurl = 'https://graph-'      # The URL of your smart home piston/rule that the 
 POtoken = 'yourTokenHere'     # Your Pushover account Token ID
 POuser = 'yourUserKey'        # Your Pushover User Key
 ifturl = 'iftttUrlHere'       # Your IFTTT URL
-picQuality = 20               # Value range is between 10-100 (default:20)
+picQuality = 15               # Value range is between 10-100 (default:20)
 vflip = '-vf'                 # To vertical flip your picture enter "-vf", otherwise leave blank ("")
 hflip = '-hf'                 # To horizontally flip your picture enter "-hf", otherwise leave blank ("")
