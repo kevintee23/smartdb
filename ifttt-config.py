8ba10365fdc29e120238e2642f0da54f4c89e1e7
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-05-27 11:12:48 +1000

    Update ifttt-config.py

--- ifttt-config.py
+++ ifttt-config.py
@@ -8,3 +8,22 @@ ifturl = config.get('App Settings', 'ifturl')
 iftEvent = config.get('App Settings', 'iftEvent')
 
 print("Leave blank (by hitting ENTER) if you do not intend to change any of the values") 
+
+#IFTTT URL
+try:
+        new_ifturl=input("Enter/paste your IFTTT Maker URL: ")
+except SyntaxError:
+        new_ifturl=config.get('App Settings', 'ifturl')
+
+config.set('App Settings', 'ifturl', new_ifturl)
+
+#IFTTT Event Name
+try:
+        new_iftEvent=input("[IFTTT Event Name: %s] Event Name: " % iftEvent)
+except SyntaxError:
+        new_iftEvent=config.get('App Settings', 'iftEvent')
+
+config.set('App Settings', 'iftEvent', new_iftEvent)
+
+with open('config1.ini', 'w') as configfile:
+	config.write(configfile)
